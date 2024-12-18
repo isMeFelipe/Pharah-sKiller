ec5afee850cf30b6eae891e1b6df6380ec55ea75
Felipe Moura <felipemoura.developer@gmail.com>
2024-12-18 14:13:28 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,5 +1,64 @@
+# Criar Venv
+python3 -m venv novo_venv
+
+# Rodar Venv
+source venv/bin/activate
+
+# Sair do Venv
+deactivate
+
+# LabelImg
+Ferramenta para rotular imagens para treinamento do YOLO
+
+    # Rodar
+     python labelImg.py
+
+
+
+
+
 # Pharah-sKiller
 Projeto da disciplina de Visão Computacional em que se tem como objetivo: 
 
 -- Detectar Pharah
 -- Prever Movimentação de Pharah
+                
+
+# Treinar
+yolo task=detect \
+mode=train \
+model=yolov8s.pt \
+data=/var/www/html/Pharah-sKiller/YOLO/datasetv2/data.yaml \
+epochs=100 \
+imgsz=416
+batch=8
+
+yolo task=detect \
+mode=train \
+model=yolov8s.pt \
+data=/var/www/html/Pharah-sKiller/YOLO/datasetv3/data.yaml \
+epochs=100 \
+imgsz=416 \
+batch=8
+
+
+
+# Rodar
+yolo task=detect \
+mode=predict \
+model=/var/www/html/Pharah-sKiller/runs/detect/train9/weights/best.pt \
+source=/var/www/html/Pharah-sKiller/YOLO/test-dataset/pharaVideoTest.mkv \
+imgsz=640 \
+conf=0.25 \
+save=True
+
+
+yolo task=detect \
+mode=predict \
+model=/var/www/html/Pharah-sKiller/runs/detect/train16/weights/best.pt \
+source=/var/www/html/Pharah-sKiller/YOLO/test-dataset/v1.mp4 \
+imgsz=416 \
+conf=0.25 \
+save=True
+
+
---
+++ SEGMENTAR/dividirEmFrame.py
@@ -0,0 +1,49 @@
+import cv2
+import os
+
+# Caminho do vídeo de entrada
+video_path = 'v1.mp4'
+
+# Caminho para a pasta onde os frames serão salvos
+frames_folder = 'frames'
+
+# Cria a pasta "frames" se não existir
+if not os.path.exists(frames_folder):
+    os.makedirs(frames_folder)
+
+# Carrega o vídeo
+cap = cv2.VideoCapture(video_path)
+
+# Verifica se o vídeo foi carregado corretamente
+if not cap.isOpened():
+    print(f"Erro ao abrir o vídeo {video_path}")
+    exit()
+
+# Obtém a taxa de frames por segundo (FPS) do vídeo
+fps = cap.get(cv2.CAP_PROP_FPS)
+print(f"FPS do vídeo: {fps}")
+
+frame_count = 0
+frame_interval = 30  # Intervalo para capturar frames (1 segundo)
+
+# Loop para ler o vídeo frame por frame
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break  # Fim do vídeo
+
+    # Verifica se o frame atual é o próximo a ser salvo
+    if frame_count % frame_interval == 0:
+        # Define o caminho para salvar o frame
+        frame_filename = os.path.join(frames_folder, f'frame_{frame_count // frame_interval:04d}.jpg')
+
+        # Salva o frame como imagem
+        cv2.imwrite(frame_filename, frame)
+
+    frame_count += 1
+
+# Libera o objeto de captura do vídeo
+cap.release()
+
+print(f"Frames extraídos e salvos na pasta {frames_folder}")
